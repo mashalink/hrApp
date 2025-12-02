82ea393378bb38dabdad1625f5ace1ad86df98bf
Maria <maria8link@gmail.com>
2025-12-02 13:16:47 +0200

    WIP

--- hrApp-frontend/src/hooks/useAxios.js
+++ hrApp-frontend/src/hooks/useAxios.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.PROD ? "https://" : "http://localhost:3001";
+const API_URL = "https://hrapp-ovc7.onrender.com";
 
 const useAxios = () => {
   const get = (path) => axios.get(`${API_URL}${path}`);
   const post = (path, data) => axios.post(`${API_URL}${path}`, data);
   const patch = (path, data) => axios.patch(`${API_URL}${path}`, data);
-
   return { get, post, patch };
 };
 
